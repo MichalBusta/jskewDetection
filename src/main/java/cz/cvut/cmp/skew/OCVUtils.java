011d6a955afecb5ed38fa3507430bc2d32a1588f
Michal Busta <michal.busta@gmail.com>
2015-11-02 11:05:22 +0100

    opencv example with affine transformation

---
+++ src/main/java/cz/cvut/cmp/skew/OCVUtils.java
@@ -0,0 +1,43 @@
+package cz.cvut.cmp.skew;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.highgui.Highgui;
+
+public class OCVUtils {
+
+	static void showImage(Mat img){
+		
+		MatOfByte matOfByte = new MatOfByte();
+	    Highgui.imencode(".jpg", img, matOfByte);
+		byte[] byteArray = matOfByte.toArray();
+		
+		BufferedImage bufImage = null;
+	    try {
+	        InputStream in = new ByteArrayInputStream(byteArray);
+	        bufImage = ImageIO.read(in);
+	        JFrame frame = new JFrame();
+	        
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        JDialog dlg = new JDialog(frame, true);
+	        dlg.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
+	        dlg.pack();
+	        dlg.setVisible(true);
+	        frame.dispose();
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+		
+	}
+
+}
---
+++ src/main/java/cz/cvut/cmp/skew/TestOpenCVSkew.java
@@ -0,0 +1,39 @@
+package cz.cvut.cmp.skew;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+public class TestOpenCVSkew {
+	
+	static {
+		nu.pattern.OpenCV.loadShared();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//read image
+		Mat img = Highgui.imread(TestOpenCVSkew.class.getResource("/TimesNewRoman-Italic-sixsided.bin.png").getPath());
+		//show image in window 
+		OCVUtils.showImage(img);
+		
+		//create transformation matrix (eye = identity)
+		Mat m =  Mat.eye(2, 3, CvType.CV_32FC1);
+		Mat dst= new Mat();
+		Imgproc.warpAffine(img, dst, m, img.size());
+		//after transformation, we expect to get same image 
+		OCVUtils.showImage(dst);
+		
+		//now we set some skew value  .... 
+		m.put(0, 1, Math.tan(Math.PI / 16) );
+		Imgproc.warpAffine(img, dst, m, img.size(), Imgproc.INTER_LINEAR, Imgproc.BORDER_CONSTANT, new Scalar(255, 255, 255));
+		// and display result  
+		OCVUtils.showImage(dst);
+	}
+
+}
