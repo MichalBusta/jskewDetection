e91bf6919db742b14f26937eaa5d7a23909c9d40
Klara Janouskova <klara.janou@gmail.com>
2015-11-10 20:00:09 +0100

    entropy calculation

--- src/main/java/cz/cvut/cmp/skew/VerticalDominant.java
+++ src/main/java/cz/cvut/cmp/skew/VerticalDominant.java
@@ -1,12 +1,12 @@
 package cz.cvut.cmp.skew;
 
-import org.opencv.core.Mat;
+import org.opencv.core.*;
 import org.opencv.highgui.Highgui;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
+
+import java.awt.*;
 
 import static org.opencv.core.Core.bitwise_not;
 import static org.opencv.core.Core.reduce;
@@ -19,22 +19,80 @@ public class VerticalDominant {
     static {
         nu.pattern.OpenCV.loadShared();
     }
+
     public static void main(String[] args) {
 
         //read image
-    Mat img = Highgui.imread("src/main/resources/TimesNewRoman-Italic-sixsided.bin.png");
-    Mat img2 = Highgui.imread("src/main/resources/TimesNewRoman-Italic-sixsided.bin.png", IMREAD_GRAYSCALE)
+        Mat img = Highgui.imread("src/main/resources/TimesNewRoman-Italic-sixsided.bin.png", Highgui.IMREAD_GRAYSCALE);
 
-    // invert the colours
-    Mat invImg = new Mat();
-    bitwise_not(img, invImg);
+        // invert the colours
+        Mat invImg = new Mat();
+        bitwise_not(img, invImg);
 
-    // check the result
-    OCVUtils.showImage(img);
-    OCVUtils.showImage(invImg);
+        // check the result
+        OCVUtils.showImage(img);
+        OCVUtils.showImage(invImg);
 
-    // create vertical projection
-    Mat rowSumImg = new Mat();
-    reduce(invImg,rowSumImg, 0, CV_REDUCE_SUM);
+        // create vertical projection
+        Mat rowSumImg = new Mat();
+        Core.reduce(invImg, rowSumImg, 0, Core.REDUCE_SUM, CvType.CV_32FC1);
+
+        System.out.println();
+
+        //calculate the entropy
+        double entropie = 0;
+        double pn = invImg.rows() * 255;
+        double pi;
+
+        double pom;
+        float[] data = new float[3];
+        for (int i = 0; i < rowSumImg.cols(); i++) {
+            rowSumImg.get(0, i, data);
+            System.out.print((int) data[0]);
+            System.out.print(" ");
+            pi = data[0];
+
+            if (pi / pn != 0) {
+                System.out.println("log:" + Math.log(pi / pn));
+                pom = (pi / pn) * Math.log(pi / pn);
+                entropie = entropie + pom;
+            }
+
+        }
+        System.out.println();
+        System.out.println("Entropie puvodniho:" + (-entropie));
+        System.out.println();
+
+
+        Mat m = Mat.eye(2, 3, CvType.CV_32FC1);
+        m.put(0, 1, Math.tan(Math.PI / 16));
+        Mat rightSkew = new Mat();
+        Imgproc.warpAffine(invImg, rightSkew, m, invImg.size());
+        OCVUtils.showImage(rightSkew);
+
+        Mat rowSumImg2 = new Mat();
+        Core.reduce(rightSkew, rowSumImg2, 0, Core.REDUCE_SUM, CvType.CV_32FC1);
+
+
+        // calculate the entropy
+        entropie = 0;
+        pn = rightSkew.rows() * 255;
+        float[] data2 = new float[3];
+        for (int i = 0; i < rowSumImg.cols(); i++) {
+            rowSumImg.get(0, i, data2);
+            System.out.print((int) data2[0]);
+            System.out.print(" ");
+            pi = data[0];
+
+            if (pi / pn != 0) {
+                System.out.println("log:" + Math.log(pi / pn));
+                pom = (pi / pn) * Math.log(pi / pn);
+                entropie = entropie + pom;
+            }
+
+
+        }
+        System.out.println();
+        System.out.println("Entropie narovnaneho:" + (-entropie));
     }
 }
