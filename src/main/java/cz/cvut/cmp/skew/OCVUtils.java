b972fff5c8323c1b152c8780cf2a65c71719876f
Klara Janouskova <klara.janou@gmail.com>
2015-11-19 15:12:20 +0100

    skew calculation

--- src/main/java/cz/cvut/cmp/skew/OCVUtils.java
+++ src/main/java/cz/cvut/cmp/skew/OCVUtils.java
@@ -16,28 +16,28 @@ import org.opencv.highgui.Highgui;
 
 public class OCVUtils {
 
-	static void showImage(Mat img){
-		
-		MatOfByte matOfByte = new MatOfByte();
-	    Highgui.imencode(".jpg", img, matOfByte);
-		byte[] byteArray = matOfByte.toArray();
-		
-		BufferedImage bufImage = null;
-	    try {
-	        InputStream in = new ByteArrayInputStream(byteArray);
-	        bufImage = ImageIO.read(in);
-	        JFrame frame = new JFrame();
-	        
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        JDialog dlg = new JDialog(frame, true);
-	        dlg.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
-	        dlg.pack();
-	        dlg.setVisible(true);
-	        frame.dispose();
-	    } catch (Exception e) {
-	        throw new RuntimeException(e);
-	    }
-		
-	}
+    static void showImage(Mat img) {
+
+        MatOfByte matOfByte = new MatOfByte();
+        Highgui.imencode(".jpg", img, matOfByte);
+        byte[] byteArray = matOfByte.toArray();
+
+        BufferedImage bufImage = null;
+        try {
+            InputStream in = new ByteArrayInputStream(byteArray);
+            bufImage = ImageIO.read(in);
+            JFrame frame = new JFrame();
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            JDialog dlg = new JDialog(frame, true);
+            dlg.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
+            dlg.pack();
+            dlg.setVisible(true);
+            frame.dispose();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
