42d105dcc0b407363889d3bf75beca5f6192cf40
Klara Janouskova <klara.janou@gmail.com>
2015-11-25 13:27:23 +0100

    Merge branch 'master' of https://github.com/MichalBusta/jskewDetection
    Conflicts:
    	.idea/workspace.xml
    	src/main/java/cz/cvut/cmp/skew/OCVUtils.java
    	src/main/java/cz/cvut/cmp/skew/VerticalDominant.java
    	src/test/java/cz/cvut/cmp/skew/TestOCVOperations.java
    
    Update samples

--- src/main/java/cz/cvut/cmp/skew/Samples.java
+++ src/main/java/cz/cvut/cmp/skew/Samples.java
@@ -10,6 +10,8 @@ import org.opencv.imgproc.Imgproc;
 
 import javax.swing.*;
 
+import java.awt.*;
+
 import static org.opencv.core.Core.bitwise_not;
 
 /**
@@ -65,11 +67,13 @@ public class Samples {
 
         // add a line plot to the PlotPanel
         plot.addLinePlot("Entropy and angle relation chart", x, y);
-        plot.setName("The relation between skew angle and entropy of an image"); //not sure what this is uspposed to do
+        plot.setName("The relation between skew angle and entropy of an image"); //not sure what this is supposed to do
 
 
         //customize the x axis
+        plot.setFixedBounds(1, -45, 45);
         plot.setAxisLabel(0, "ANGLE");
+
 
         // cusotmize the y axis
         plot.setFixedBounds(1, 4.2, 4.8);
@@ -84,12 +88,56 @@ public class Samples {
 
     }
 
+    public void getVerticalProjectionImage(Mat img) {
+        double[] y; // vert. projection values
+        double[] x; // hist. columns
+
+        // invert the colours
+        Mat invImg = new Mat();
+        bitwise_not(img, invImg);
+        // get the histogram matrix
+        Mat hist = new Mat();
+        Core.reduce(invImg, hist, 0, Core.REDUCE_SUM, CvType.CV_32FC1);
+
+        // put the histogram values into an array
+        System.out.println(hist.cols());
+        y = new double[hist.cols()];
+        x = new double[hist.cols()];
+        float[] data = new float[3];
+        for (int i = 0; i < hist.cols(); i++) {
+            hist.get(0, i, data);
+            y[i] = data[0];
+            x[i] = i;
+        }
+        for (int i = 0; i < x.length; i++) {
+            System.out.println(x[i] + " " + y[i]);
+        }
+
+
+        // create the histogram plot
+        Plot2DPanel plot = new Plot2DPanel();
+        plot.addLinePlot("Vertical projection profiles", x, y);
+
+        //customize the x axis
+        plot.setFixedBounds(0, 0, 105);
+
+        // cusotmize the y axis (intervals)
+
+
+        JFrame frame = new JFrame("a plot panel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JDialog dlg = new JDialog(frame, true);
+        dlg.setContentPane(plot);
+        dlg.pack();
+        dlg.setVisible(true);
+    }
+
     public void getSampleImages(Mat img) {
         //original
         OCVUtils.showImage(img);
         //extend image with border on left and right side of the image
         Scalar value = new Scalar(255);
-        Imgproc.copyMakeBorder(img, img, 0, 0, img.rows(), img.rows(), Imgproc.BORDER_CONSTANT, value);
+        Imgproc.copyMakeBorder(img, img, 0, 0, 2 * img.rows(), 2 * img.rows(), Imgproc.BORDER_CONSTANT, value);
 
         Mat edited = new Mat();
         // correct skew
@@ -107,14 +155,24 @@ public class Samples {
 
     public static void main(String[] args) {
 
+        Samples samples = new Samples();
+
         // read image
         Mat img = Highgui.imread("src/main/resources/TimesNewRoman-Italic-sixsided.bin.png", Highgui.IMREAD_GRAYSCALE);
 
-        // get the relation chart
-        Samples samples = new Samples();
+
+        // get vertical projection profile of the initial skew
+        samples.getVerticalProjectionImage(img);
+
+        // get vertical projection profile of the correct skew
+        Mat edited = new Mat();
+        SkewEstimator.skewImage(img, edited, Math.toRadians(14));
+        samples.getVerticalProjectionImage(edited);
+
+        // get the realtion between entropy and skew angle chart
         samples.getRelationChart(img);
 
-        // get the images
+        //get sample images for skew
         samples.getSampleImages(img);
     }
 }
