6c334507fee0dc92f006464b711e4ae8c1c92c71
Klara Janouskova <klara.janou@gmail.com>
2016-02-21 22:52:38 +0100

    Merge branch 'master' of https://github.com/MichalBusta/jskewDetection
    Conflicts:
    	.idea/workspace.xml
    	src/main/java/cz/cvut/cmp/skew/OCVUtils.java
    	src/main/java/cz/cvut/cmp/skew/VerticalDominant.java
    	src/test/java/cz/cvut/cmp/skew/TestOCVOperations.java
    
    ContourSkewEstimator

--- src/test/java/cz/cvut/cmp/skew/TestContourSkew.java
+++ src/test/java/cz/cvut/cmp/skew/TestContourSkew.java
@@ -1,6 +1,7 @@
 package cz.cvut.cmp.skew;
 
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
@@ -16,12 +17,14 @@ public class TestContourSkew {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Mat img = Highgui.imread("src/main/resources/Arial-Bold-VARIOUS.bin.png", Highgui.IMREAD_GRAYSCALE);
+		Mat img = Highgui.imread("src/main/resources/TimesNewRoman-Italic-sixsided.bin.png", Highgui.IMREAD_GRAYSCALE);
 		Mat skew = new Mat();
 
 		
 		ContourSkewEstimator cs = new ContourSkewEstimator();
-		SkewEstimator.skewImageWBG(img, skew, Math.toRadians(-10));
+		Imgproc.copyMakeBorder(img, img, 0, 0, img.rows(), img.rows(), Imgproc.BORDER_CONSTANT, Scalar.all(255));
+		OCVUtils.showImage(img);
+		SkewEstimator.skewImageWBG(img, skew, Math.toRadians(0));
 		cs.estimateSkew(skew);
 
 	}
